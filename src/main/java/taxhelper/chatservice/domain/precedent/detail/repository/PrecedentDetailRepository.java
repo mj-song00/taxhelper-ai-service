0a6fa1cae4d974e07068e579d80f00910517ebbb
mj-song00 <niveus128@gmail.com>
2026-06-02 18:19:26 +0900

    refactor(ai):  Spring 파일 제거

--- src/main/java/taxhelper/chatservice/domain/precedent/detail/repository/PrecedentDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package taxhelper.chatservice.domain.precedent.detail.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import taxhelper.chatservice.domain.precedent.detail.entity.PrecedentDetail;
-
-public interface PrecedentDetailRepository extends JpaRepository<PrecedentDetail, String> {
-
-
-}
